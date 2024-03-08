2c7bea4f3652244b0bfb8664c1544c6b46daeda8
lauchavez1 <2202053@utrivieramaya.edu.mx>
2024-03-08 03:29:22 -0500

    first commit

---
+++ lib/view/listview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:practica_lista_navegacion/model/task.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(task.title),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Center(child: Text(task.description)),
+    );
+  }
+}
